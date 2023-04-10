58b0c21d775a13400cbbbebc5f1b92ee3836f36a
Yeap Sheng Kit <kit010315@gmail.com>
2023-04-10 21:57:32 +0800

    updates to encapsulation (for VotingModule) voter now can directly cast vote by calling .addVote

--- src/Entity/Voter.java
+++ src/Entity/Voter.java
@@ -1,8 +1,11 @@
 package Entity;
+import MyADT.*;
 
 public class Voter {
     private int id;
     private String name;
+    private HashSet<String> votedBook;
+    private VotingModule voteMap;
 
     public Voter() {
         int id;
@@ -12,6 +15,14 @@ public class Voter {
     public Voter(int id, String name) {
         this.id = id;
         this.name = name;
+        this.votedBook = new HashSet<>();
+    }
+
+    public Voter(int id, String name, VotingModule voteMap) {
+        this.id = id;
+        this.name = name;
+        this.votedBook = new HashSet<>();
+        this.voteMap = voteMap;
     }
 
     public int getId() {
@@ -29,15 +40,31 @@ public class Voter {
     public void setName(String name) {
         this.name = name;
     }
+
+    public HashSet<String> getVotedBook() {
+        return votedBook;
+    }
+
+    public void setVotedBook(HashSet<String> votedBook) { this.votedBook = votedBook;}
 // getter and setter methods for id and name go here
 
 
-    public void addVote(){
+    public void addVote(String book){
         //store as set
-
+        votedBook.add(book);
+        this.voteMap.addVote(book,this);
     }
 
-    public void removeVote(){
+    public void removeVote(String book){
         //remove
+        HashSet<String> voterList = voteMap.showVoters(book);
+        votedBook.remove(book);
+        voterList.remove(this.getName());
+    }
+
+    public void showRanking(){
+        for(String i: voteMap.showVotedBooks()){
+            System.out.println(i + " : " + voteMap.countVotes(i));
+        }
     }
 }
--- src/Entity/VotingModule.java
+++ src/Entity/VotingModule.java
@@ -3,9 +3,9 @@ package Entity;
 import MyADT.*;
 
 public class VotingModule {
-    public MyHashMap<String, HashSet<String>> voteMap = new MyHashMap<>();
+    private MyHashMap<String, HashSet<String>> voteMap = new MyHashMap<>();
     //    public Set<String> voterSet = new HashSet<>();
-    public Voter voters = new Voter();
+//    public Voter voters = new Voter();
 
     public VotingModule(){
         MyHashMap<String, HashSet<Voter>> voteMap = new MyHashMap<>();
@@ -22,7 +22,6 @@ public class VotingModule {
             voterSet.add(voterName);
             voteMap.put(book,voterSet);
         }
-        System.out.println(voteMap.get(book));
     }
 
 
@@ -32,17 +31,26 @@ public class VotingModule {
     }
 
     public int countVotes(String book){
-        HashSet<String> voterName = voteMap.get(book);
-        int count =voterName.size();
-        return count;
+        if(voteMap.containsKey(book)){
+            HashSet<String> voterName = voteMap.get(book);
+            return voterName.size();
+        }
+        else{
+            return 0;
+        }
     }
 
-    public HashSet<String> showVotedBooks(){
+    public String[] showVotedBooks(){
         HashSet<String> votedBooks = voteMap.getKeys();
-        return votedBooks;
+        return votedBooks.toArray();
     }
 
     public void cancelBook(String book){
         voteMap.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return voteMap.toString();
     }
 }
