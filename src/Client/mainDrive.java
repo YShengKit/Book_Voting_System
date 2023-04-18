992527062729350066693aefd8c31de21adfa231
Yeap Sheng Kit <kit010315@gmail.com>
2023-04-18 20:24:55 +0800

    update maindrive function

--- src/Client/mainDrive.java
+++ src/Client/mainDrive.java
@@ -8,10 +8,20 @@ import Entity.VotingModule;
 import MyADT.*;
 
 public class mainDrive {
+    private static MyHashMap<Integer, Voter> voters = new MyHashMap<>();
+    private static Voter currentVoter = null;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         VotingModule voteMap = new VotingModule();
-        Voter voter1 = new Voter(1, "John", voteMap);
+        // Create some sample voters
+        Voter voter1 = new Voter(123, "Alice",voteMap);
+        Voter voter2 = new Voter(456, "Bob",voteMap);
+        Voter voter3 = new Voter(234,"john",voteMap);
+
+        voters.put(voter1.getId(), voter1);
+        voters.put(voter2.getId(), voter2);
+        voters.put(voter3.getId(), voter3);
+
         HashSet<String> books = new HashSet<>();
 
         MyHashMap<String, HashSet<String>> book_cat= new MyHashMap<>();
@@ -19,6 +29,8 @@ public class mainDrive {
         books.add("book1");
         books.add("book2");
         books.add("book3");
+        books.add("book4");
+        books.add("book5");
 
         HashSet<String> cat1 = new HashSet<>();
         cat1.add("cat1");
@@ -30,34 +42,76 @@ public class mainDrive {
         book_cat.put("book1",cat1); //cat1 and cat2
         book_cat.put("book2",cat2); //cat3 and cat4
         book_cat.put("book3",cat2); //cat3 and cat4
+        book_cat.put("book5",cat2);
+
+        // Login loop
+        Scanner sc = new Scanner(System.in);
+        while (currentVoter == null) {
+            System.out.println("Enter voter ID:");
+            int id = sc.nextInt();
+            System.out.println("Enter voter name:");
+            String name = sc.next();
+
+            Voter voter = voters.get(id);
+            if (voter != null && voter.getName().equals(name)) {
+                currentVoter = voter;
+                System.out.println("Login successful. Welcome, " + currentVoter.getName() + "!");
+            } else {
+                System.out.println("Incorrect voter ID or name. Please try again.");
+            }
+
+            while(currentVoter!=null){
+                System.out.println("Hi " + currentVoter.getName() + ", welcome to Book Voting System\n 1. vote \n 2. check rank \n 3) logout");
+                int menuChoices = sc.nextInt();
+                if (menuChoices ==1){
+                    VotingModule vm = new VotingModule();
+                    HashSet<String> temp_cat_list = vm.displayCategory(book_cat);
+                    for (int i = 0; i < temp_cat_list.size(); i++) {
+                        System.out.println((i+1) + ". " + temp_cat_list.toArray()[i]);
+                    }
+
+                    System.out.println("Which category u want to choose?(use no.)");
+                    int cat_choice = sc.nextInt();
+
+                    String category = temp_cat_list.toArray()[cat_choice - 1] ;
+
+                    HashSet<String> temp_book_lists = vm.displayBook(book_cat, category);
+                    for (int i=0; i<temp_book_lists.size(); i++){
+                        System.out.println((i+1) + ". " + temp_book_lists.toArray()[i]);
+                    }
+
+                    System.out.println("Which book u want to vote?");
+                    int choice_book = sc.nextInt();
+
+                    currentVoter.addVote(temp_book_lists.toArray()[choice_book-1]);
+
+//                    String[] abc = voteMap.showVotedBooks();
+//                    for (int i = 0; i < abc.length; i++) {
+                    System.out.println("You have voted for "+ currentVoter.getVotedBook());
+//                    }
+
+                }else if (menuChoices==2){
+                    currentVoter.showRanking();
+                }else if(menuChoices == -1) {
+                    break;
+                }else{
+                    //logout
+                    System.out.println("proceed to logout");
+                    currentVoter = null;
+                }
 
 
-        VotingModule vm = new VotingModule();
-        HashSet<String> temp_cat_list = vm.displayCategory(book_cat);
-        for (int i = 0; i < temp_cat_list.size(); i++) {
-            System.out.println((i+1) + ". " + temp_cat_list.toArray()[i]);
+
+            }
+//        System.out.println("Currently logged in as: " + currentVoter.getName());
+
+
+
+            // Do whatever you need to do with the logged-in voter here
+
+
         }
 
-        System.out.println("Which category u want to choose?(use no.)");
-        int cat_choice = sc.nextInt();
-
-        String category = temp_cat_list.toArray()[cat_choice - 1] ;
-
-        HashSet<String> temp_book_lists = vm.displayBook(book_cat, category);
-        for (int i=0; i<temp_book_lists.size(); i++){
-            System.out.println((i+1) + ". " + temp_book_lists.toArray()[i]);
-        }
-
-        System.out.println("Which book u want to vote?");
-        int choice_book = sc.nextInt();
-
-        voter1.addVote(temp_book_lists.toArray()[choice_book-1]);
-
-        String[] abc = voteMap.showVotedBooks();
-        for (int i = 0; i < abc.length; i++) {
-            System.out.println(abc[i]);
-        }
-        voter1.showRanking();
     }
 
 }
